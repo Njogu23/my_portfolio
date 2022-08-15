9ab000a6b2c696412012c421f8f3cafba1f6314f
George Njogu <sbeveme@gmail.com>
2022-08-15 21:19:17 +0300

    js initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const nav = document.querySelectorAll('.nav');
+
+function navMouseOver(){
+   nav.addEventListener('mouseover', function(){
+      nav.style.background = 'gold';
+   })
+}
+navMouseOver();
+
+function navMouseLeave(){
+   nav.addEventListener('mouseleave', function(){
+      nav.style.background = 'none';
+   })
+}
+navMouseLeave();
+
 
